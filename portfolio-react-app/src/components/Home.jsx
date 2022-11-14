19da2fc73fca2224fd9e7f90b566c19f2fbded62
Joe Bentley <joe.bentley.20@gmail.com>
2022-11-14 13:54:18 +0000

    update home component

--- portfolio-react-app/src/components/Home.jsx
+++ portfolio-react-app/src/components/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-slate-600'>Hi, my name is</p>
+        <p className='text-slate-600'></p>
         <h1 className='text-4xl sm:text-7xl font-bold text-white'>Joe Bentley</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-slate-500'>I'm a Junior Web Developer</h2>
+        <h2 className='text-4xl mt-4 sm:text-7xl font-bold text-slate-500'>Junior Web Developer</h2>
         <p className='text-slate-500 py-4 max-w-[700px]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit rerum provident eaque dolor, quasi facilis ullam odit, quod, veniam eius sapiente dolorem quisquam! Reiciendis ad fugit, distinctio repellat facilis laborum?</p>
         <div>
           <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-slate-500 '>View more 
