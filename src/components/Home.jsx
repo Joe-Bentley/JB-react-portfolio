ce30fab55835bda04a8c317b95f3af4f37699b44
Joe Bentley <joe.bentley.20@gmail.com>
2026-02-22 15:11:56 +0000

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
         <p className='text-slate-600'></p>
         <h1 className='text-4xl font-bold sm:text-7xl'>Joe Bentley</h1>
         <h2 ref={typeRef} className='text-2xl mt-4 sm:text-5xl font-bold text-[#FF0000] dark:text-slate-500 type'></h2>
-        <p className='text-[#1C1C1C] dark:text-gray-300 py-4 max-w-[700px]'>This portfolio is a collection of the work I’ve built, the problems I’ve solved, and the projects I’ve enjoyed along the way. It’s designed and developed using React and Tailwind. Have a look around — and if something sparks your interest, <a className='underline transition-opacity duration-300 hover:opacity-70 underline-offset-2' href="mailto:joe.bentley.20@gmail.com">let’s chat!</a></p>
+        <p className='text-[#1C1C1C] dark:text-gray-300 py-4 max-w-[700px]'>This portfolio is a collection of the work I’ve built, the problems I’ve solved, and the projects I’ve enjoyed along the way. It’s designed and developed using React and Tailwind. Have a look around and if something sparks your interest, <a className='underline transition-opacity duration-300 hover:opacity-70 underline-offset-2' href="mailto:joe.bentley.20@gmail.com">let’s chat!</a></p>
         <div>
           <ul className='flex flex-row gap-2 py-4 sm:text-2xl'>
             <li className='hover:text-[#FF0000] hover:translate-y-[-4px] duration-500'><a href="mailto:joe.bentley.20@gmail.com"><ImMail2/></a></li>
